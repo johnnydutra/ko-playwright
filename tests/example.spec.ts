a0bb2daa595977df8fe538ad37146e0962d8463a
Johnny Dutra <johnny.dutra@gmail.com>
2024-08-13 12:01:55 -0300

    click on element

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,3 +5,12 @@ test('Simple basic test', async ({ page }) => {
   const pageTitle = await page.locator('h1')
   await expect(pageTitle).toContainText('Example Domain')
 })
+
+test('Clicking on elements', async ({ page }) => {
+  await page.goto('http://zero.webappsecurity.com/index.html')
+  await page.click('#signin_button')
+  await page.click('text=Sign in')
+  
+  const errorMessage = await page.locator('.alert-error')
+  await expect(errorMessage).toContainText('Login and/or password are wrong.')
+})
