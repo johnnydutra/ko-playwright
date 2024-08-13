fd915a7ec9c9bb4ede231e54df0205005fdac2ae
Johnny Dutra <johnny.dutra@gmail.com>
2024-08-13 12:10:06 -0300

    selectors

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -14,3 +14,22 @@ test('Clicking on elements', async ({ page }) => {
   const errorMessage = await page.locator('.alert-error')
   await expect(errorMessage).toContainText('Login and/or password are wrong.')
 })
+
+test.skip('Selectors', async ({ page }) => {
+  // text
+  await page.click('text=some text')
+
+  // css selectors
+  await page.click('button')
+  await page.click('#id')
+  await page.click('.class')
+
+  // only visible css selector
+  await page.click('.submit-button:visible')
+
+  // combinations
+  await page.click('#username .first')
+
+  // xpath
+  await page.click('//button')
+  })
