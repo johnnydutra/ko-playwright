d949f2716306d0a24086fdf2b312029ce982aad4
Johnny Dutra <johnny.dutra@gmail.com>
2024-08-13 12:28:51 -0300

    annotations

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -34,27 +34,29 @@ test.skip('Selectors', async ({ page }) => {
   await page.click('//button')
   })
 
-test('Working with inputs', async ({ page }) => {
-  await page.goto('http://zero.webappsecurity.com/index.html')
-  await page.click('#signin_button')
-  await page.fill('#user_login', 'some username')
-  await page.fill('#user_password', 'some password')
-  await page.click('text=Sign in')
-
-  const errorMessage = await page.locator('.alert-error')
-  await expect(errorMessage).toContainText('Login and/or password are wrong.')
-})
-
-test('Assertions', async ({ page }) => {
-  await page.goto('https://www.example.com')
-  await expect(page).toHaveURL('https://www.example.com')
-  await expect(page).toHaveTitle('Example Domain')
-
-  const element = await page.locator('h1')
-  await expect(element).toBeVisible()
-  await expect(element).toHaveText('Example Domain')
-  await expect(element).toHaveCount(1)
-
-  const nonExistingElement = await page.locator('h5')
-  await expect(nonExistingElement).not.toBeVisible()
+test.describe('My first test suite', () => {
+  test('Working with inputs', async ({ page }) => {
+    await page.goto('http://zero.webappsecurity.com/index.html')
+    await page.click('#signin_button')
+    await page.fill('#user_login', 'some username')
+    await page.fill('#user_password', 'some password')
+    await page.click('text=Sign in')
+  
+    const errorMessage = await page.locator('.alert-error')
+    await expect(errorMessage).toContainText('Login and/or password are wrong.')
+  })
+  
+  test('Assertions', async ({ page }) => {
+    await page.goto('https://www.example.com')
+    await expect(page).toHaveURL('https://www.example.com')
+    await expect(page).toHaveTitle('Example Domain')
+  
+    const element = await page.locator('h1')
+    await expect(element).toBeVisible()
+    await expect(element).toHaveText('Example Domain')
+    await expect(element).toHaveCount(1)
+  
+    const nonExistingElement = await page.locator('h5')
+    await expect(nonExistingElement).not.toBeVisible()
+  })
 })
