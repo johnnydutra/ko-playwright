d811f920c03eb2a721979fd53a4002629d7936b3
Johnny Dutra <johnny.dutra@gmail.com>
2024-08-14 12:34:00 -0300

    get request test

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, request } from '@playwright/test'
 
 test.describe.parallel('API Testing', () => {
   const baseUrl = 'https://reqres.in/api'
@@ -13,5 +13,16 @@ test.describe.parallel('API Testing', () => {
   test('Simple API Test - Assert Invalid Endpoint', async ({ request }) => {
     const response = await request.get(`${baseUrl}/users/non-existing-endpoint`)
     expect(response.status()).toBe(404)
+  })
+
+  test('GET Request - Get User Detail', async ({ request }) => {
+    const response = await request.get(`${baseUrl}/users/1`)
+    const responseBody = JSON.parse(await response.text())
+
+    expect(response.status()).toBe(200)
+    expect(responseBody.data.id).toBe(1)
+    expect(responseBody.data.first_name).toBe('George')
+    expect(responseBody.data.last_name).toBe('Bluth')
+    expect(responseBody.data.email).toBeTruthy()
   })
 })
