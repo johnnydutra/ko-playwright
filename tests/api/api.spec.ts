d9f670de12bcfbc8d861bbd98ecb18c588905b74
Johnny Dutra <johnny.dutra@gmail.com>
2024-08-14 12:27:28 -0300

    parse json response data

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -6,6 +6,8 @@ test.describe.parallel('API Testing', () => {
   test('Simple API Test - Assert Response Status', async ({ request }) => {
     const response = await request.get(`${baseUrl}/users/2`)
     expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
   })
 
   test('Simple API Test - Assert Invalid Endpoint', async ({ request }) => {
