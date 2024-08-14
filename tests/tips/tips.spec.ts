166fbe4caf729101c87a11c2db40b1fc7e055fa4
Johnny Dutra <johnny.dutra@gmail.com>
2024-08-14 14:53:10 -0300

    mouse simulation

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -25,5 +25,13 @@ test.describe('Tips & Tricks Section', () => {
     })
   }
 
+  test('Mouse Movement Simulation', async ({ page }) => {
+    await page.goto('https://www.example.com')
+    await page.mouse.move(0, 0)
+    await page.mouse.down()
+    await page.mouse.move(0, 100)
+    await page.mouse.up()
+  })
+
 
 })
