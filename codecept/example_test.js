00424ab8d0429763114e02e0486a61f9599183d1
Johnny Dutra <johnny.dutra@gmail.com>
2024-08-15 10:05:40 -0300

    assertions

--- codecept/example_test.js
+++ codecept/example_test.js
@@ -3,4 +3,7 @@ Feature('example')
 Scenario('Should load example.com website',  ({ I }) => {
   I.amOnPage('https://www.example.com')
   I.see('Example')
+  I.dontSee('Google')
+  I.seeElement('h1')
+  I.dontSeeElement('#idontexist')
 })
